e082f8dcec543c827767918227502f380c7bab71
Vincent Robert <vincent.robert@nanego.com>
2015-06-01 18:06:46 +0200

    Update Rspec version

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,14 +1,24 @@
 require "spec_helper"
 require 'redmine_email_address_exporter/projects_controller_patch'
 
-describe ProjectsController do
+describe ProjectsController, :type => :controller do
+  render_views
 
   it "should get_mail_addresses" do
     # TODO
   end
 
   it "should query_to_mail_addresses" do
+    # TODO
+  end
 
+  it "should see link to modal on /projects page" do
+    User.current = User.find(1) # admin
+    get :index
+    expect(response).to be_success
+    assert_select "a.atom[href='/projects.atom']"
+    #TODO: make this test work ; seems deface overrides are not included in test environment (?)
+    # assert_select "p.other-formats", :count => 2
   end
 
 end
